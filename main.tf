7ef882407bfbb81cd15bdd3743db27bacda020af
Leonardo Roese <leo.roese@hotmail.com>
2021-10-28 12:36:41 -0500

    add depends on example

--- main.tf
+++ main.tf
@@ -20,6 +20,19 @@ provider "aws" {
   region  = var.aws_region
 }
 
+resource "aws_iam_role_policy" "example" {
+  name = "example"
+  role = aws_iam_role.example.name
+  policy = jsonencode({
+    "Statement" = [{
+      # This policy allows software running on the EC2 instance to
+      # access the S3 API.
+      "Action" = "s3:*",
+      "Effect" = "Allow",
+    }],
+  })
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-074cce78125f09d61"
   instance_type = var.TF_VAR_instance_type
@@ -27,4 +40,8 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = var.TF_VAR_instance_name
   }
+
+  depends_on = [
+    aws_iam_role_policy.example
+  ]
 }
