3db81b928d1de8194f3d8f5b7167397471976810
Leonardo Roese <leo.roese@hotmail.com>
2021-10-28 15:57:47 -0500

    test

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "instance_type" {
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "Updated Name"
+  default     = "Updated Name For VCS Test"
 }
 
 variable "aws_region" {
