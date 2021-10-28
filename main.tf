eadc0fa33cbdf926c7d6b1b2f74c2a525e7f5613
Leonardo Roese <leo.roese@hotmail.com>
2021-10-28 16:07:41 -0500

    depends on

--- main.tf
+++ main.tf
@@ -21,11 +21,48 @@ provider "aws" {
   region  = var.aws_region
 }
 
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "example" {
+  name = "example"
+  role = aws_iam_role.test_role.name
+  policy = jsonencode({
+    "Statement" = [{
+      # This policy allows software running on the EC2 instance to
+      # access the S3 API.
+      "Action" = "s3:*",
+      "Effect" = "Allow",
+    }],
+  })
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0ba62214afa52bec7"
+  ami           = "ami-074cce78125f09d61"
   instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
   }
+
+  depends_on = [
+    aws_iam_role_policy.example
+  ]
 }
