642713177d2fc0ea1ef269e4e64a0515a06df6a7
Herbert <habingwa@gmail.com>
2023-07-06 19:46:24 -0400

    Test Update

--- variables.tf
+++ variables.tf
@@ -1,25 +1,11 @@
 
+#Creating an input variable
 variable "instance_type" {
-  description = "Value of the instance type for the EC2 instance"
-  type        = string
-  default     = "t2.micro"
+  type = string
+  default = "t2.micro"
 }
 
-
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "Updated Name"
-}
-
-variable "aws_region" {
-  description = "Value of aws default region"
-  type        = string
-  default     = "us-east-2"
-}
-
-variable "aws_profile" {
-  description = "Value of aws profile"
-  type        = string
-  default     = "terraform"
+variable "azs" {
+  type = list(string)
+  default = [ "us-east-2a", "us-east-2b", "us-east-2c" ]
 }
